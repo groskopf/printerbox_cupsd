8b089b0fb5d7f413ddba197a30acb6c2d01b852d
Paul Groskopf <paul@groskopf-embedded.com>
2021-04-02 19:48:04 +0200

    Don't loop but use docker compose -d

--- scripts/start_cupsd.sh
+++ scripts/start_cupsd.sh
@@ -4,4 +4,4 @@ set -x
 /usr/sbin/cupsd
 lpadmin -p TD4550DNWB -E -v usb://Brother/TD-4520DN?serial=000A0Z694367 -P /usr/share/cups/model/Brother/brother_td4550dnwb_printer_en.ppd
 
-while true; do sleep 1; done
+#while true; do sleep 1; done
