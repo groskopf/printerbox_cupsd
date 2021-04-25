8d50ac50892a5f57fba082ba10a877b70a700dad
Paul Groskopf <paul@groskopf-embedded.com>
2021-04-25 10:26:46 +0200

    cupsd docker forbliver i live

--- scripts/start_cupsd.sh
+++ scripts/start_cupsd.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 set -x
+set -e
 
-/usr/sbin/cupsd
+# Start cupsd
+/usr/sbin/cupsd -f &
+sleep 2
+
+#Add printer
 lpadmin -p TD4550DNWB -E -v usb://Brother/TD-4520DN?serial=000A0Z694367 -P /usr/share/cups/model/Brother/brother_td4550dnwb_printer_en.ppd
+lpstat -a
+cancel -a
 
-#while true; do sleep 1; done
+set +x
+# Wait for cupsd to stop
+CUPSD_PID=$(cat /var/run/cups/cupsd.pid)
+while [ -d "/proc/${CUPSD_PID}" ]; do sleep 1; done
