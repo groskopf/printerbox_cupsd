468b2dc9c291e52022d81bf384f3fd2f46277b40
groskopf <paul@groskopf-embedded.com>
2021-05-19 20:40:09 +0100

    Generate label file with number

--- scripts/install_labels.sh
+++ scripts/install_labels.sh
@@ -3,6 +3,5 @@ set -x
 
 /usr/sbin/cupsd
 
-brpapertoollpr_td4550dnwb -P TD4550DNWB -n navneskilt -w 98 -h 170.7 -g 3.0 -b 3.0 -t 3.0 -r 1.5 -l 1.5 -S 2 -m 3.3 -o 0.0
-lpoptions -p TD4550DNWB -o BrTrimtape=OFF
-cat /etc/cups/ppd/TD4550DNWB.ppd | grep navneskilt | grep PageSize | sed 's/.* \(.*\)\/.*/\1/' > labels/navneskilt.txt
+brpapertoollpr_td4550dnwb -P TD4550DNWB -n navneskilt_4786103 -w 98 -h 170.7 -g 3.0 -b 3.0 -t 3.0 -r 1.5 -l 1.5 -S 2 -m 3.3 -o 0.0
+cat /etc/cups/ppd/TD4550DNWB.ppd | grep navneskilt_4786103 | grep PageSize | sed 's/.* \(.*\)\/.*/\1/' > labels/4786103.txt
