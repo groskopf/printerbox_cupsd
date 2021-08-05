c7a6323683a7c50c3646b0c6fd2b4fada949cd2f
Paul Groskopf <paul@groskopf-embedded.com>
2021-08-05 21:47:58 +0200

    Adjust 4786103 label size

--- scripts/install_labels.sh
+++ scripts/install_labels.sh
@@ -3,5 +3,5 @@ set -x
 
 /usr/sbin/cupsd
 
-brpapertoollpr_td4550dnwb -P TD4550DNWB -n navneskilt_4786103 -w 98 -h 170.7 -g 3.0 -b 3.0 -t 3.0 -r 1.5 -l 1.5 -S 2 -m 3.3 -o 0.0
+brpapertoollpr_td4550dnwb -P TD4550DNWB -n navneskilt_4786103 -w 103 -h 171 -g 0 -b 3 -t 3 -r 1.5 -l 1.5 -S 2 -m 3.0 -o 0.0
 cat /etc/cups/ppd/TD4550DNWB.ppd | grep navneskilt_4786103 | grep PageSize | sed 's/.* \(.*\)\/.*/\1/' > labels/4786103.txt
